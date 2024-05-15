1cf1d81e487cfe8bc2bff5f97adc0f8ce4b09ae1
Lotfi <lotfi1002@gmail.com>
2024-05-15 16:09:33 +0100

    axios

--- src/pages/ListProductsPage.js
+++ src/pages/ListProductsPage.js
@@ -1,9 +1,19 @@
 import {db} from '../models/db'
+import React, { useState } from 'react';
+import axios from "axios";
 import { useLiveQuery } from "dexie-react-hooks";
 
 export const ProductList = () => {
+    //const [resp, setResp] = useState('');
     const products = useLiveQuery(() => db.product.toArray());
 
+    function componentDidMount(){
+        axios.get('http://localhost/PWA-POS/api/users').then((response) => {
+            console.log(response.data) ; 
+            });
+    };
+
+    componentDidMount();
     return (
         <>
         <h1> List Of product </h1>
@@ -12,7 +22,9 @@ export const ProductList = () => {
             <li key={product.id}>
               {product.libelle}, {product.pu}
             </li>
+        
           ))}
+       
         </ul>
         </>
       );
